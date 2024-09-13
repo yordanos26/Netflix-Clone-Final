08f940e6da056b7227822d8fadf8f3b1f3b805d9
Yordanos Mamuya <yordanosd2@gmail.com>
2024-09-12 23:41:49 -0400

    banner

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import Banner1 from "../../Components/Banner/Banner1";
+// import RowList from "../../Components/Rows/RowList/RowList";
+
+
 
 const Home = () => {
   return (
     <>
       <Header />
-      <Footer/>
+      <Banner1/>
+      <Footer />
     </>
   );
 };
